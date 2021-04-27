1dd6e5f874117f5d4e3e2c30e7bb8d4197b03419
yimo <moyishi213@gmail.com>
2021-04-27 10:37:32 +0800

    use erl:scan

--- script/trace_tools.erl
+++ script/trace_tools.erl
@@ -4,65 +4,51 @@
 
 -export([main/1]).
 
-%% 因为我懒得搞include的解析了,麻烦您把除了本项目include外的依赖路径写在这里
-%% 另外请把本脚本的路径也写进去
--define(Include, [
-
-]).
-
 main([FilePath, LineInt]) ->
-    {unix, darwin} = os:type(),
-    c:c(trace_tools_parse),
-    Line = list_to_integer(LineInt),
-    Ast = ast(FilePath),
-    Module = lists:foldl(fun({attribute, _, module, M}, _) -> M;(_, M) -> M end, '_', Ast),
-    FunctionMap = lists:reverse(parse_function(Ast, [])),
-    case search_function(Line, FunctionMap) of
-        null -> ok;
-        Function ->
-            Cmd = recon_trace_format(Module, Function),
-            os:cmd("echo \"" ++ Cmd ++ "\" | pbcopy"),
-            ok
-    end.
 
-search_function(Line, [{Function1, Line1}, {_Function2, Line2} | _]) when Line1 =< Line andalso Line =< Line2 ->
-    Function1;
-search_function(_Line, [{Function, _}]) ->
-    Function;
-search_function(Line, [_ | Rest]) ->
-    search_function(Line, Rest);
-search_function(_Line, []) ->
-    null.
+    Base = filename:basename(FilePath),
+    true = (length(Base) - 4 > 0),
+    Module = lists:sublist(Base, length(Base) - 4),
+    Base = Module ++ ".erl",
+    {Function, _, Body} = find(new_function(FilePath), list_to_integer(LineInt)),
+    Cmd = io_lib:format("recon_trace:calls({~p,~p,fun" ++ token_to_code(Body, []) ++ "-> return_trace() end},100,[{scope,local}]).", [list_to_atom(Module), Function]),
+    os:cmd("echo \"" ++ lists:flatten(Cmd) ++ "\" | pbcopy").
 
-parse_function([{function, Line, Function, _Args, _Body} | Rest], Acc) ->
-    case proplists:is_defined(Function, Acc) of
-        true -> parse_function(Rest, Acc);
-        false -> parse_function(Rest, [{Function, Line} | Acc])
-    end;
-parse_function([_ | Rest], Acc) ->
-    parse_function(Rest, Acc);
-parse_function([], Acc) ->
+token_to_code([{Symbol, _} | Token], Acc) when is_atom(Symbol) ->
+    token_to_code(Token, [atom_to_list(Symbol) | Acc]);
+token_to_code([{_, _, Arg} | Token], Acc) ->
+    token_to_code(Token, [atom_to_list(Arg) | Acc]);
+token_to_code([], Acc) ->
+    lists:concat(lists:reverse(Acc)).
+
+
+
+find([{F, L1, B} | _Rest], Line) when L1 > Line ->
+    {F, L1, B};
+find([{F, L1, B}, {_, L2, _} | _Rest], Line) when L1 =< Line andalso Line < L2 ->
+    {F, L1, B};
+find([Last], _Line) ->
+    Last;
+find([_ | Rest], Line) ->
+    find(Rest, Line).
+
+
+to_token(File) when 1 =:= 1 ->
+    {ok, Code} = file:read_file(File),
+    {ok, Token, _} = erl_scan:string(binary_to_list(Code)),
+    Token.
+
+new_function(File) ->
+    lists:reverse(p_new_function(to_token(File), [])).
+p_new_function([{Split, _}, {atom, L, F} | R], Acc) when Split =:= 'dot' orelse Split =:= ';' ->
+    {Function, Rest} = parse_function(R, []),
+    p_new_function(Rest, [{F, L, Function} | Acc]);
+p_new_function([_ | Rest], Acc) ->
+    p_new_function(Rest, Acc);
+p_new_function([], Acc) ->
     Acc.
-recon_trace_format(Module, Function) ->
-    Tmp = io_lib:format("recon_trace:calls({~p,~p,fun(_)-> return_trace() end},100,[{scope,local}]).", [Module, Function]),
-    lists:flatten(Tmp).
 
-ast(Path) ->
-    erlang:register(?MODULE, self()),
-    io:format("~p~n", [include(Path) ++ [{parse_transform, trace_tools_parse}]]),
-    c:c(Path, include(Path) ++ [{parse_transform, trace_tools_parse}]),
-    receive AST -> AST after 1000 -> throw(compile_error) end.
-
-include(Path) ->
-    ProjectDir = to_project_dir(Path),
-    I = lists:map(fun(Dep) -> {i, filename:join([ProjectDir, Dep])} end, ?Include),
-    [{i, filename:join([ProjectDir, "include"])}] ++ I.
-
-to_project_dir(Path) ->
-    case filename:basename(Path) of
-        <<>> -> throw({path_error, Path});
-        [] -> throw({path_error, Path});
-        "src" -> filename:dirname(Path);
-        "test" -> filename:dirname(Path);
-        _ -> to_project_dir(filename:dirname(Path))
-    end.
+parse_function([{'->', _} | Rest], Func) ->
+    {lists:reverse(Func), Rest};
+parse_function([Any | Rest], Acc) ->
+    parse_function(Rest, [Any | Acc]).
